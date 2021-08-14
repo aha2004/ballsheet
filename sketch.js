a757f9d228182e211d3b34494fea13ae1ac4384a
aha2004 <88874663+aha2004@users.noreply.github.com>
2021-08-14 05:11:29 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -25,7 +25,7 @@ let overall = 0;
 let EPSRANGE = 5;
 let W = 800;
 let H = 800;
-let BALLSIZE = 30;
+let BALLSIZE = 60;
 let CURSORSIZE = 30;
 let time;
 let score;
@@ -115,13 +115,13 @@ function preload()
     ballsHS = getItem(6)/1000;   
    }
 
-  eatsound = loadSound("idk.wav");
+  eatsound = loadSound("bop.mp3");
 }
 
 let cnv;
 let ctx;
 function setup() {
-  frameRate(1000);
+  frameRate(1960);
   TARGETCOLOR = color(255,0,0);
   BACKGROUNDCOLOR = color(7,7,7);
   MOUSECOLOR = color(255,255,255);
