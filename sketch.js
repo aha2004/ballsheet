5cc91345edd8ee9b82e6e8c59fb3f30282395252
aha2004 <88874663+aha2004@users.noreply.github.com>
2021-08-22 10:18:32 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -121,8 +121,7 @@ function preload()
 let cnv;
 let ctx;
 function setup() {
-  frameRate(2240
-	   );
+  frameRate(2340);
   TARGETCOLOR = color(255,0,0);
   BACKGROUNDCOLOR = color(7,7,7);
   MOUSECOLOR = color(255,255,255);
